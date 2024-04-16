869ef0d53514166c64447284ddf87ed1bc9f7cf9
Andrey Kirik <dev.andrey.kirik@gmail.com>
2024-04-16 23:33:35 +0500

    feat: update deps, install local storage

---
+++ lib/storage/item_type.dart
@@ -0,0 +1,8 @@
+class ItemType {
+  static String language = 'language';
+  static String theme = 'theme';
+  static String guideIsDone = 'guideIsDone';
+  static String ownerId = 'ownerId';
+  static String accessToken = 'accessToken';
+  static String refreshToken = 'refreshToken';
+}
