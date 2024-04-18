a1149e0cfb3daba7179c9253b301a88a7f4f9f17
Andrey Kirik <dev.andrey.kirik@gmail.com>
2024-04-19 00:16:45 +0500

    feat: add headers of lang selecting page

---
+++ lib/pages/lang_select.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class LangSelect extends StatefulWidget {
+  const LangSelect({super.key});
+
+  @override
+  State<LangSelect> createState() => _LangSelectState();
+}
+
+class _LangSelectState extends State<LangSelect> {
+  String language = 'en';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: [
+            SizedBox(height: 128),
+            Center(
+              child: Text('Select Language'),
+            ),
+            Center(
+              child: Text('Configure your PetID, by selecting app language. \n*If you understand this, you are on the right way*'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
