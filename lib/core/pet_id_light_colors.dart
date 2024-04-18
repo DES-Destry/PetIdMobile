8119c3e9329c727cdfe2be3786dc6fb96a55b7de
Andrey Kirik <dev.andrey.kirik@gmail.com>
2024-04-19 00:04:30 +0500

    feat: make app loader stateful

---
+++ lib/core/pet_id_light_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class PetIdLightColors {
+  static const Color primary = Color(0xFF1DE24A);
+  static const Color background = Color(0xFFDDDDDD);
+  static const Color accent = Color(0xFF828282);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:pet_id_mobile/core/pet_id_dark_colors.dart';
+import 'package:pet_id_mobile/core/pet_id_light_colors.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const AppLoader());
 }
 
-class AppLoader extends StatelessWidget {
+class AppLoader extends StatefulWidget {
   const AppLoader({super.key});
 
+  @override
+  State<AppLoader> createState() => _AppLoaderState();
+}
+
+class _AppLoaderState extends State<AppLoader> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PetID',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: PetIdLightColors.primary),
+        scaffoldBackgroundColor: PetIdLightColors.background,
         useMaterial3: true,
       ),
-      darkTheme: ThemeData(),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: PetIdDarkColors.primary),
+        scaffoldBackgroundColor: PetIdDarkColors.background,
+        useMaterial3: true,
+      ),
     );
+  }
+
+  void loadAppFromLastState() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
   }
 }
