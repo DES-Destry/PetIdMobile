01faae538121a0679a3cc8692b626aa1e4d9b279
Andrey Kirik <dev.andrey.kirik@gmail.com>
2024-06-11 04:06:37 +0500

    fix: homepage fixes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -26,7 +26,7 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         backgroundColor: AppPalette.currentPalette.primary,
         foregroundColor: AppPalette.currentPalette.white,
-        leading: null,
+        automaticallyImplyLeading: false,
         title: Text(currentTabTitle.tr(),
             textAlign: TextAlign.center,
             style: const TextStyle(
@@ -42,120 +42,130 @@ class _HomePageState extends State<HomePage> {
             filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
             child: Container(
               decoration: BoxDecoration(
-                color: Colors.grey.shade200.withOpacity(0.5),
+                color: Colors.grey.shade200.withOpacity(0.1),
                 border: Border(
                     top: BorderSide(
                         color: AppPalette.currentPalette.grayBorder,
                         width: 1.0)),
               ),
-              child: Row(
+              child: Column(
                 children: [
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        currentTab = PetListTab();
-                        currentTabIndex = 0;
-                        currentTabTitle = 'tab.yourPets';
-                      });
-                    },
-                    child: Column(
+                  const SizedBox(height: 10.0),
+                  SizedBox(
+                    height: 71,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        Image.asset(
-                            Paths.getImage(currentTabIndex == 0
-                                ? 'pets-tab-active.png'
-                                : 'pets-tab.png'),
-                            width: 28,
-                            height: 28),
-                        const SizedBox(height: 4),
-                        Text(
-                          'tab.yourPets'.tr(),
-                          style: TextStyle(
-                              fontSize: 10,
-                              fontWeight: FontWeight.bold,
-                              color: currentTabIndex == 0
-                                  ? AppPalette.currentPalette.primary
-                                  : AppPalette.currentPalette.grayBorder),
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              currentTab = PetListTab();
+                              currentTabIndex = 0;
+                              currentTabTitle = 'tab.yourPets';
+                            });
+                          },
+                          child: Column(
+                            children: [
+                              Image.asset(
+                                  Paths.getImage(currentTabIndex == 0
+                                      ? 'pets-tab-active.png'
+                                      : 'pets-tab.png'),
+                                  width: 28,
+                                  height: 28),
+                              const SizedBox(height: 4),
+                              Text(
+                                'tab.yourPets'.tr(),
+                                style: TextStyle(
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.bold,
+                                    color: currentTabIndex == 0
+                                        ? AppPalette.currentPalette.primary
+                                        : AppPalette.currentPalette.grayBorder),
+                              )
+                            ],
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            // Open scanner
+                          },
+                          child: Column(
+                            children: [
+                              Image.asset(Paths.getImage('scan-qr-tab.png'),
+                                  width: 28, height: 28),
+                              const SizedBox(height: 4),
+                              Text(
+                                'tab.scanQr'.tr(),
+                                style: TextStyle(
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.bold,
+                                    color:
+                                        AppPalette.currentPalette.grayBorder),
+                              )
+                            ],
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              // currentTab = PetListTab();
+                              currentTabIndex = 2;
+                              currentTabTitle = 'tab.notifications';
+                            });
+                          },
+                          child: Column(
+                            children: [
+                              Image.asset(
+                                  Paths.getImage(currentTabIndex == 2
+                                      ? 'notifications-tab-active.png'
+                                      : 'notifications-tab.png'),
+                                  width: 28,
+                                  height: 28),
+                              const SizedBox(height: 4),
+                              Text(
+                                'tab.notifications'.tr(),
+                                style: TextStyle(
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.bold,
+                                    color: currentTabIndex == 2
+                                        ? AppPalette.currentPalette.primary
+                                        : AppPalette.currentPalette.grayBorder),
+                              )
+                            ],
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              // currentTab = PetListTab();
+                              currentTabIndex = 3;
+                              currentTabTitle = 'tab.profile';
+                            });
+                          },
+                          child: Column(
+                            children: [
+                              Image.asset(
+                                  Paths.getImage(currentTabIndex == 3
+                                      ? 'profile-tab-active.png'
+                                      : 'profile-tab.png'),
+                                  width: 28,
+                                  height: 28),
+                              const SizedBox(height: 4),
+                              Text(
+                                'tab.profile'.tr(),
+                                style: TextStyle(
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.bold,
+                                    color: currentTabIndex == 3
+                                        ? AppPalette.currentPalette.primary
+                                        : AppPalette.currentPalette.grayBorder),
+                              )
+                            ],
+                          ),
                         )
                       ],
                     ),
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      // Open scanner
-                    },
-                    child: Column(
-                      children: [
-                        Image.asset(Paths.getImage('scan-qr-tab.png'),
-                            width: 28, height: 28),
-                        const SizedBox(height: 4),
-                        Text(
-                          'tab.scanQr'.tr(),
-                          style: TextStyle(
-                              fontSize: 10,
-                              fontWeight: FontWeight.bold,
-                              color: AppPalette.currentPalette.grayBorder),
-                        )
-                      ],
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        // currentTab = PetListTab();
-                        currentTabIndex = 2;
-                        currentTabTitle = 'tab.notifications';
-                      });
-                    },
-                    child: Column(
-                      children: [
-                        Image.asset(
-                            Paths.getImage(currentTabIndex == 2
-                                ? 'notifications-tab-active.png'
-                                : 'notifications-tab.png'),
-                            width: 28,
-                            height: 28),
-                        const SizedBox(height: 4),
-                        Text(
-                          'tab.notifications'.tr(),
-                          style: TextStyle(
-                              fontSize: 10,
-                              fontWeight: FontWeight.bold,
-                              color: currentTabIndex == 2
-                                  ? AppPalette.currentPalette.primary
-                                  : AppPalette.currentPalette.grayBorder),
-                        )
-                      ],
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        // currentTab = PetListTab();
-                        currentTabIndex = 3;
-                        currentTabTitle = 'tab.profile';
-                      });
-                    },
-                    child: Column(
-                      children: [
-                        Image.asset(
-                            Paths.getImage(currentTabIndex == 3
-                                ? 'profile-tab-active.png'
-                                : 'profile-tab.png'),
-                            width: 28,
-                            height: 28),
-                        const SizedBox(height: 4),
-                        Text(
-                          'tab.profile'.tr(),
-                          style: TextStyle(
-                              fontSize: 10,
-                              fontWeight: FontWeight.bold,
-                              color: currentTabIndex == 3
-                                  ? AppPalette.currentPalette.primary
-                                  : AppPalette.currentPalette.grayBorder),
-                        )
-                      ],
-                    ),
-                  )
                 ],
               ),
             ),
